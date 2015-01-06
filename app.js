accecad48786c8e96b755fe5e50a1c882b9b4254
lei-clearsky <sportzhulei@gmail.com>
2015-01-06 10:57:15 -0500

    add user input validation related functions

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var guessGame = (function ($){
 
 	// Begin module scope variables
 	var userInput,
+		previousInput,
 		userInputInt,
 		game,
 		initModule;
@@ -13,6 +14,7 @@ var guessGame = (function ($){
 	// Game class
 	var Game = function() {
 		this.guessLeft = 5;
+		this.previousInput = 0;
 		this.randomNum = Math.floor(Math.random() * 100 + 1);
 		$('#remainGuesses').html(this.guessLeft);
 		console.log(this.randomNum);
@@ -22,13 +24,47 @@ var guessGame = (function ($){
 	// user submit guess, verify user input
 	Game.prototype.submitGuess = function () {
 		userInput = $('#userGuess').val();
-		this.checkAnswer(userInput);
+		if (this.evaluateInput(userInput))
+			console.log(userInput);
+			this.checkAnswer(userInput);
+	}
+
+	// verify if user's input is valid
+	Game.prototype.evaluateInput = function (input) {
+		if (isNaN(input) || input > 100 || input < 0) {
+			$('#hint-info').html("Please enter a valid number and hit submit.");
+			return false;
+		} 
+		
+		return true;	
+	}
+
+	// compare previous input and current user input
+	// depend on the difference of the two values, provide feedback
+	Game.prototype.checkTemp = function (input) {
+		console.log('Input: ' + input);
+		console.log('this.previousInput: ' + this.previousInput);
+		var diff = Math.abs(input - this.previousInput);
+
+		if (diff <= 5)
+			$('#temp-info').html("Super Hot!");
+		else if (diff <= 10)
+			$('#temp-info').html("Hot!");
+		else if (diff <= 15)
+			$('#temp-info').html("Warm!");
+		else if (diff <= 25)
+			$('#temp-info').html("Cold!");
+		else 
+			$('#temp-info').html("Ice Cold!");
 
 	}
-	// verify user input
+
+	// check user's valid input answer
 	Game.prototype.checkAnswer = function(input) {
 
-		userInputInt = parseInt(input);
+		this.checkTemp(input);
+
+		this.previousInput = input;
 
 		if (this.guessLeft === 0){
 			$('#hint-info').html("You've used all your guesses!");
